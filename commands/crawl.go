ca62f7863d426bd82bef0a05078ef111a6f6f3f3
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-12-25 22:18:09 +0000

    Refactor crawler setup to worker.

--- commands/crawl.go
+++ commands/crawl.go
@@ -2,63 +2,15 @@ package commands
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
+	"github.com/ipfs-search/ipfs-search/commands/crawlworker"
 	"github.com/ipfs-search/ipfs-search/config"
-	"github.com/ipfs-search/ipfs-search/crawler"
-	"github.com/ipfs-search/ipfs-search/extractor/tika"
-	"github.com/ipfs-search/ipfs-search/index/elasticsearch"
 	"github.com/ipfs-search/ipfs-search/instr"
-	"github.com/ipfs-search/ipfs-search/protocol/ipfs"
-	"github.com/ipfs-search/ipfs-search/queue/amqp"
-	t "github.com/ipfs-search/ipfs-search/types"
-	"github.com/ipfs-search/ipfs-search/worker"
-	samqp "github.com/streadway/amqp"
 
 	"log"
 	// "go.opentelemetry.io/otel/api/trace"
 	// "go.opentelemetry.io/otel/codes"
 )
-
-type consumeChans struct {
-	Files       <-chan samqp.Delivery
-	Directories <-chan samqp.Delivery
-	Hashes      <-chan samqp.Delivery
-}
-
-func getConsumeChans(ctx context.Context, cfg *config.Config, instrumentation *instr.Instrumentation) (*consumeChans, error) {
-	var c consumeChans
-
-	queues, err := getQueues(ctx, cfg, instrumentation)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Files, err = queues.Files.Consume(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Directories, err = queues.Directories.Consume(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	c.Hashes, err = queues.Hashes.Consume(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return &c, nil
-}
-
-func makeWorkers(ctx context.Context, consumeChan <-chan samqp.Delivery, c *crawler.Crawler, n uint) {
-	var i uint
-	for i = 0; i < n; i++ {
-		go work(ctx, consumeChan, c)
-	}
-}
 
 // Crawl configures and initializes crawling
 func Crawl(ctx context.Context, cfg *config.Config) error {
@@ -68,23 +20,14 @@ func Crawl(ctx context.Context, cfg *config.Config) error {
 	}
 	defer instFlusher()
 
-	instrumentation := instr.New()
-	tracer := instrumentation.Tracer
+	instr := instr.New()
+	tracer := instr.Tracer
 
 	ctx, span := tracer.Start(ctx, "commands.Crawl")
 	defer span.End()
 
-	f := factory.New()
-	c, err := f.Crawler(ctx)
-	if err != nil {
-		return err
-	}
-
-	consumeChans, err := getConsumeChans(ctx, cfg, instrumentation)
-
-	makeWorkers(ctx, consumeChans.Files, c, cfg.Workers.FileWorkers)
-	makeWorkers(ctx, consumeChans.Hashes, c, cfg.Workers.HashWorkers)
-	makeWorkers(ctx, consumeChans.Directories, c, cfg.Workers.DirectoryWorkers)
+	c := crawlworker.New(cfg, instr)
+	c.Start(ctx)
 
 	// Context closure or panic is the only way to stop crawling
 	<-ctx.Done()
